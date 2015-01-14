2404c10b02bec267fff50f328a0892df25760f08
Eric Williams <eric@churchwilliams.com>
2015-01-13 22:04:55 -0700

    pretty much finished

--- quizApp.js
+++ quizApp.js
@@ -5,27 +5,38 @@
 
 //Create a User constructor that accepts name, email, password, and totalScore parameters and set them appropriatly
 
-  //code here
+var User = function(name, email, password, totalScore) {
+	this.name = name;
+	this.email = email;
+	this.password = password;
+	this.totalScore = totalScore;
+};
 
 
 //Create a Question constructor that accepts title, answersArray, rightAnswer, and difficulty parameters
 
-  //code here
+var Question = function(title, answersArray, rightAnswer) {
+	this.title = title;
+	this.answersArray = answersArray;
+	this.rightAnswer = rightAnswer;
+};
 
 
 //Create a users Array which is going to hold all of our users.
 
-  //code here
+var users = [];
 
 
 //Let's say three people signed up for our service, create 3 instances of User and add each to the users Array
 
-  //code here
+var user1 = new User('eric', 'edogg@gmail.com', 'cabroncito', 80);
+var user2 = new User('sombryn', 'sombryn@gmail.com', 'perro', 81);
+var user3 = new User('remy', 'remsky@gmail.com', 'elmo', 82);
 
 
 //Create a questions Array which is going to hold all of our questions
 
-  //code here
+var questions = [];
 
 
 //Now, let's say we wanted to create a quiz about JavaScript. Create three instances of Question which contain the following data
@@ -34,7 +45,7 @@
 //title: "T/F: In Javascript, == doesn't check 'type' but just the value - where === checks type and value"
 //Fill in the rest of the required data as you see appropriate.
 
-  //code here
+var q1 = new Question('T/F: Inheritance is achieved in JavaScript through Prototypes?');
 
 
 //Now push all of your instances of Question into the questions Array
